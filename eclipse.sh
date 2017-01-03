fc065c02572315b55583e8553785cc428f8eaa2a
sonnt85 <nts2201@gmail.com>
2017-01-03 20:52:52 +0700

    Update eclipse.sh
    share all /dev

--- eclipse.sh
+++ eclipse.sh
@@ -2,6 +2,7 @@
 BRIDGE_NAME=dockersonnt
 docker network ls | grep ${BRIDGE_NAME} &>/dev/null || docker network create\
    -o "com.docker.network.bridge.name"="${BRIDGE_NAME}" --subnet=172.18.0.0/16 ${BRIDGE_NAME}
+ #      --privileged -v /dev/bus/usb:/dev/bus/usb \
 function start_eclipse(){
 docker ps -a | grep eclipse || {
       docker pull sonnt/eclipse
@@ -12,7 +13,7 @@ docker ps -a | grep eclipse || {
       --restart always \
       -v /home/sonnt/workspace:/home/sonnt/workspace \
       -v /tmp/.X11-unix:/tmp/.X11-unix\
-      --privileged -v /dev/bus/usb:/dev/bus/usb \
+      --privileged -v /dev:/dev \
       -e DISPLAY=:0\
       -tid sonnt/eclipse nohup /opt/eclipse/eclipse  &>/dev/null&
     }
