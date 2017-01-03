2fb683fb84c672d56313d84914c6f5faf48158b7
sonnt85 <nts2201@gmail.com>
2017-01-03 23:45:23 +0700

    Update eclipse.sh

--- eclipse.sh
+++ eclipse.sh
@@ -2,7 +2,8 @@
 BRIDGE_NAME=dockersonnt
 docker network ls | grep ${BRIDGE_NAME} &>/dev/null || docker network create\
    -o "com.docker.network.bridge.name"="${BRIDGE_NAME}" --subnet=172.18.0.0/16 ${BRIDGE_NAME}
- #      --privileged -v /dev/bus/usb:/dev/bus/usb \
+ # --privileged -v /dev/bus/usb:/dev/bus/usb \
+ # xhost +local:docker
 function start_eclipse(){
 docker ps -a | grep eclipse || {
       docker pull sonnt/eclipse
